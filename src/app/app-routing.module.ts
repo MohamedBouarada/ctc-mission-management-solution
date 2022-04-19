5cbae46136bebdf2bffbce0af4cea2ad6fa67ad7
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2022-04-19 21:37:38 +0100

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanningComponent } from './componnents/planning/planning.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupOrganismComponent } from './pages/signup-organism/signup-organism.component';
 import { SignupUserComponent } from './pages/signup-user/signup-user.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
-  { path: 'login',component:LoginComponent},
-  { path: 'signup-user',component:SignupUserComponent},
-  { path: 'signup-organism',component:SignupOrganismComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'signup-user', component: SignupUserComponent },
+  { path: 'signup-organism', component: SignupOrganismComponent },
+  { path: 'planning', component: PlanningComponent },
 ];
 
 @NgModule({
