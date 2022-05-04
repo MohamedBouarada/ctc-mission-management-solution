89e7756d2454294137e66c11ba079254f3832b1a
Racem Benrhayem <racembenrhayem@insat.u-cartage.tn>
2022-05-04 16:57:54 +0100

    fix/enrollment : relations options + extra infos column

--- src/enrollment/entities/enrollment.entity.ts
+++ src/enrollment/entities/enrollment.entity.ts
@@ -1,44 +1,50 @@
-import { Course } from "src/courses/entities/course.entity";
-import { timeStamp } from "src/shared/time-stamp";
-import { User } from "src/user/entities/user.entity";
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { statesEnum } from "../enums/states.enum";
-import { userTypesEnum } from "../enums/user-types.enum";
+import { Course } from 'src/courses/entities/course.entity';
+import { timeStamp } from 'src/shared/time-stamp';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { statesEnum } from '../enums/states.enum';
+import { userTypesEnum } from '../enums/user-types.enum';
 
 @Entity()
-export class Enrollment extends timeStamp{
-
-    @PrimaryGeneratedColumn('increment')
-    id:number;
-    @JoinColumn()
-    @ManyToOne(type=>User,{         //options to review
-       eager:true,
-       cascade:['update','insert'],
-       nullable:true
-    })
-    user:User;
-    @JoinColumn()
-    @ManyToOne(type=>Course,{         //options to review
-       eager:true,
-       cascade:['update','insert'],
-       nullable:true
-    })
-    course:Course;
-    @Column({
-        type: 'enum',
-        enum: statesEnum,
-        default: statesEnum.inProgress,
-      })
-      state: statesEnum;
-    @Column({
-        type: 'enum',
-        enum: userTypesEnum,
-        default: userTypesEnum.individual,
-      })
-      userType:userTypesEnum;
-    @Column({default : 1})
-    size: number;
-    @Column('json',{nullable: true})
-    extraInformations:{fullName :string, email: string}[];
-
+export class Enrollment extends timeStamp {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @JoinColumn()
+  @ManyToOne(() => User, {
+    // TODO: options to review
+    eager: true,
+    cascade: ['insert'],
+    onDelete: 'SET NULL',
+  })
+  user: User;
+  @JoinColumn()
+  @ManyToOne(() => Course, {
+    // TODO: options to review
+    eager: true,
+    cascade: ['update', 'insert'],
+    onDelete: 'SET NULL',
+  })
+  course: Course;
+  @Column({
+    type: 'enum',
+    enum: statesEnum,
+    default: statesEnum.inProgress,
+  })
+  state: statesEnum;
+  @Column({
+    type: 'enum',
+    enum: userTypesEnum,
+    default: userTypesEnum.individual,
+  })
+  userType: userTypesEnum;
+  @Column({ default: 1 })
+  size: number;
+  @Column({ nullable: true })
+  extraInformations: string;
 }
